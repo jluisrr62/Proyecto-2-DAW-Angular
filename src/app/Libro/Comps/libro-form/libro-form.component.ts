23b4d110d09f2dd4bf99d75a03bab3ea09762ac7
Jose <jluisrr62@educastur.es>
2023-06-14 20:58:51 +0200

    version para entrega

--- src/app/Libro/Comps/libro-form/libro-form.component.ts
+++ src/app/Libro/Comps/libro-form/libro-form.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { Libro } from 'src/app/Libro/libro';
 import { CrudOperationsService } from 'src/app/Config/CRUD generico/crud-operations.service';
+import { Asignatura } from '../../Asignatura/asignatura';
 
 
 @Component({
@@ -13,19 +14,44 @@ import { CrudOperationsService } from 'src/app/Config/CRUD generico/crud-operati
 export class LibroFormComponent {
   libro: Libro;
   private librosUrl: string;
+  private asignaturasUrl: string;
+  nombresAsignaturas: string[] = []; 
+  asignaturas: Asignatura[] =[];
 
   constructor(
     private route: ActivatedRoute, 
     private router: Router, 
-    private libroService: CrudOperationsService<Libro>) {
+    private libroService: CrudOperationsService<Libro>,
+    private asignaturaService: CrudOperationsService<Asignatura>) {
       this.libro = new Libro();
       this.librosUrl = 'http://localhost:8090/libros';
+      this.asignaturasUrl = 'http://localhost:8090/asignaturas';
     }
+    
+    ngOnInit(){
+      this.getAsignaturas();
+    }
+
     onSubmit() {
       this.libroService.create(this.librosUrl,this.libro).subscribe(result => this.gotolibroList());
     }
 
     gotolibroList(){
       this.router.navigate(['/libros']);
+    }
+
+    getAsignaturas(){
+      this.asignaturaService.getAll(this.asignaturasUrl).subscribe(data => {
+        this.asignaturas = data;
+      })
+    }
+
+    getNombresAsignaturas(){
+      this.nombresAsignaturas = [];
+      this.asignaturas.forEach(asignatura => {
+        this.nombresAsignaturas.push(asignatura.nombre);
+      });
+  
+      return this.nombresAsignaturas;
     }
 }
